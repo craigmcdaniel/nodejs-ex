6209ee7bb8d42c4842d7e78984a1a0e860917ed7
craigmcdaniel <38362647+craigmcdaniel@users.noreply.github.com>
2018-07-04 13:07:35 -0400

    hr query

--- server.js
+++ server.js
@@ -77,6 +77,16 @@ app.get('/', function (req, res) {
   }
 });
 
+app.get('/hr'),function (req,res){
+
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    db.collection('hr').find({});
+  }
+}
+
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
